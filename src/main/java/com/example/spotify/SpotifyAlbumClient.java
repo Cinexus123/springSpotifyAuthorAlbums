3f54003336e2e919b5109d0b1f37e8cbca9b7186
cinex96 <marcin.nelacny@gmail.com>
2019-12-27 14:06:51 +0100

    Find albums by author

---
+++ src/main/java/com/example/spotify/SpotifyAlbumClient.java
@@ -0,0 +1,47 @@
+package com.example.spotify;
+
+
+import com.example.spotify.model.SpotifyAlbum;
+import com.example.spotify.model.dto.SpotifyAlbumDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class SpotifyAlbumClient {
+
+
+    @GetMapping("/album/{authorName}")
+    public List<SpotifyAlbumDto> getAlbumsByAuthor(OAuth2Authentication details, @PathVariable String authorName) {
+
+        String jwt = ((OAuth2AuthenticationDetails)details.getDetails()).getTokenValue();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt);
+        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+
+        ResponseEntity<SpotifyAlbum> exchange = restTemplate.exchange("https://api.spotify.com/v1/search?q="+ authorName + "&type=track&market=US&limit=10&offset=5",
+                HttpMethod.GET,
+                httpEntity,
+                SpotifyAlbum.class);
+
+        List<SpotifyAlbumDto> spotifyAlbumDtos = exchange.getBody()
+                .getTracks().getItems().stream()
+                .map(item-> new SpotifyAlbumDto(item.getName(),item.getAlbum().getImages().get(0).getUrl()))
+                .collect(Collectors.toList());
+
+        return spotifyAlbumDtos;
+    }
+}
+
---
+++ src/main/java/com/example/spotify/model/dto/SpotifyAlbumDto.java
@@ -0,0 +1,34 @@
+package com.example.spotify.model.dto;
+
+public class SpotifyAlbumDto {
+
+    private String trackName;
+    private String imageUrl;
+
+    public SpotifyAlbumDto(String name) {
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public SpotifyAlbumDto(String trackName, String imageUrl) {
+        this.trackName = trackName;
+        this.imageUrl = imageUrl;
+   }
+
+    public SpotifyAlbumDto() {
+    }
+}
